1821fe0e77b335bcd32f3638e9769788bd12b777
KarenRolo <karenroolk@gmail.com>
2019-10-20 14:53:17 -0500

    Se terminan estilos de Pagina de condigurador

--- src/pages/Configuration/Configuration.jsx
+++ src/pages/Configuration/Configuration.jsx
@@ -5,29 +5,41 @@ class Configuration extends Component{
     render = () =>{
         return(
             <div class="container configurator" >
-                <div class="row">
+                <div class="row justify-content-between">
                     <div class="col-md-5 configurator_description">
-                        <span className=" configurator_title">
+                        <span className=" configurator_description_title">
                             Save more with Bellotero.io
                         </span>
                         <p>
                             With Bellotero.io you save time and money make real-time decisions that boost your business and your bottom line. Get less wrongfully blocked payments, save time on bookkeeping and no need to worry about safety. 
                         </p>
                     </div>
-                    <div class="col-md-7 configurator_calculator">
+                    <div class="col-md-6 configurator_calculator">
                         <div className="my-5">
-                            <label htmlFor="customRange1">Monthly ingredient spending</label>
+                            <label className="configurator_calculator_label">Monthly ingredient spending</label>
+                            <div className="configurator_calculator_input">
+                                <p>$</p>
+                            </div>
                             <input type="range" className="custom-range" id="customRange1" />
                         </div>
                         <div className="my-5">
-                            <label htmlFor="customRange2">Full-time employees that  process invoices</label>
+                            <label className="configurator_calculator_label">Full-time employees that  process invoices</label>
+                            <div className="configurator_calculator_input">
+                                <p>$</p>
+                            </div>
                             <input type="range" className="custom-range" id="customRange2" />
                         </div>
-                        <div class="col-md-6 result">
-
-                        </div>
-                        <div class="col-md-6 result">
-
+                        <div className="row">
+                            <div class="col-sm-6 result">
+                                <span className="configurator_calculator_simbol">$</span>
+                                <span className="configurator_calculator_quantity">36.211</span>
+                                <p>Estimated cost food savings</p>
+                            </div>
+                            <div class="col-sm-6 result">
+                                <span className="configurator_calculator_simbol">$</span>
+                                <span className="configurator_calculator_quantity">8.611</span>                            
+                                <p>Your estimated annual savings</p>
+                            </div>
                         </div>
                     </div>
                 </div>
